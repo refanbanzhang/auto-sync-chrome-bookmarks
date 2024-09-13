497a4888551f24e9e75a59c58874005a29c34b0c
refanbanzhang <gaoqiang19514@163.com>
2024-09-13 21:39:24 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('Hello from Vue!')
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style scoped>
+/* 可以在这里添加样式 */
+</style>
