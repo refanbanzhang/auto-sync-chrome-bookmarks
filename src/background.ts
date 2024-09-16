b15a1b78d83821996971c4b76b2de62401300461
refanbanzhang <gaoqiang19514@163.com>
2024-09-16 12:50:55 +0800

    feat: 修改页码长度

--- src/background.ts
+++ src/background.ts
@@ -33,3 +33,36 @@ chrome.bookmarks.onRemoved.addListener(async () => {
 });
 
 console.log('背景脚本已加载');
+
+
+// 修改 declarativeNetRequest 规则以拦截特定的 AJAX 请求
+const RULE_ID = 1;
+const TARGET_URL = "https://apiv3.shanbay.com/news/user_articles";
+const PARAM_KEY = "ipp";
+const PARAM_VALUE = "100";
+
+function createRule() {
+  return {
+    id: RULE_ID,
+    priority: 1,
+    action: {
+      type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
+      redirect: {
+        transform: {
+          queryTransform: {
+            addOrReplaceParams: [{ key: PARAM_KEY, value: PARAM_VALUE }]
+          }
+        }
+      }
+    },
+    condition: {
+      urlFilter: `${TARGET_URL}?list_type=liked&${PARAM_KEY}=10`,
+      resourceTypes: [chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST]
+    }
+  };
+}
+
+chrome.declarativeNetRequest.updateDynamicRules({
+  removeRuleIds: [RULE_ID],
+  addRules: [createRule()]
+});
